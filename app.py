05aa42780d5b489cd7ac435da74a9f15cfb7e09b
IFAIStartup <ifaistartup@gmail.com>
2024-11-27 17:59:08 +0530

    Update api key

--- app.py
+++ app.py
@@ -132,7 +132,7 @@ def detect_objects():
 def fetch_satellite_image(north, south, east, west,zoom):
    
 
-    url = f"https://maps.gomaps.pro/maps/api/staticmap?center={(north+south)/2},{(east+west)/2}&zoom={zoom}&size=928x544&maptype=satellite&key=AlzaSy56ZAnc1zfXHZW6gL4TzQ7m2xoNpyo3JkU"
+    url = f"https://maps.gomaps.pro/maps/api/staticmap?center={(north+south)/2},{(east+west)/2}&zoom={zoom}&size=928x544&maptype=satellite&key=AlzaSyIK2uORSR8IEOsnW7pB6w17mok7Fv7JkUM"
     
     response = requests.get(url)
     if response.status_code == 200:
